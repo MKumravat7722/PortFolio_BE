be6642d837e12bb46a7a7782a56583ce650be79c
mohit <mohitkumravat22@gmail.com>
2025-10-28 23:24:30 -0700

    Revert "add assests"
    This reverts commit 404d74e899c291b641adb4b3710b9ed89696399a.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,6 @@
 //= link_tree ../images
-//= link_tree ../stylesheets .css
-//= link_tree ../javascripts .js
+//= link_directory ../javascripts .js
+//= link_directory ../stylesheets .css
 //= link_tree ../fonts
+
+
